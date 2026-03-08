39c761d81ea0f58672b8e7fed5baabdae54d5ff7
Dev Suthar <4ev3rdk@gmail.com>
2026-03-08 12:10:49 +0530

    Added UI to Rock,Paper and Scissors using addEventListener

--- javascript.js
+++ javascript.js
@@ -11,62 +11,100 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let ch = prompt("Select Rock, Paper or Scissors").toLowerCase();
-    // ch = ch.toLowerCase();
-    if (ch === "rock") {
-        return "Rock";
-    }
-    else if (ch === "paper") {
-        return "Paper";
-    }
-    else if (ch === "scissors") {
-        return "Scissors";
-    }
-}
+// function getHumanChoice() {
+//     let ch = prompt("Select Rock, Paper or Scissors").toLowerCase();
+//     ch = ch.toLowerCase();
+//     if (ch === "rock") {
+//         return "Rock";
+//     }
+//     else if (ch === "paper") {
+//         return "Paper";
+//     }
+//     else if (ch === "scissors") {
+//         return "Scissors";
+//     }
+// }
 // console.log(getHumanChoice());
 // console.log(getComputerChoice());
 
 let computerScore = 0;
 let humanScore = 0;
 
-function playRound(computerChoice, humanChoice) {
+//event listener to play round for each button
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
 
+
+rock.addEventListener("click", () => {
+    let playerSelection = "Rock";
+    playRound(getComputerChoice(), playerSelection); ///let computeChoice= getcomputerchoice() then write computerChoice
+});
+paper.addEventListener("click", () => {
+    let playerSelection = "Paper";
+    playRound(getComputerChoice(), playerSelection);
+});
+scissors.addEventListener("click", () => {
+    let playerSelection = "Scissors";
+    playRound(getComputerChoice(), playerSelection)
+});
+
+
+
+
+const round = document.querySelector("#round");
+const score = document.querySelector("#score");
+const result = document.querySelector("#result");
+
+
+
+function playRound(computerChoice, humanChoice) {
+    if (humanScore >= 5 || computerScore >= 5) {
+
+        return;
+    }
 
     if (computerChoice === humanChoice) {
-        console.log(`Round tied : ${computerChoice} and ${humanChoice} cuts each other  `);
-        console.log(` You;${humanScore} Computer:${computerScore}`)
+
+
+        round.textContent = `${computerChoice} and ${humanChoice} cuts each other`;
+        score.textContent = `You:${humanScore} Computer:${computerScore}`;
+
+
     }
     else if (computerChoice === "Rock" && humanChoice == "Scissors" || computerChoice === "Paper" && humanChoice === "Rock" || computerChoice === "Scissors" && humanChoice === "Paper") {
         computerScore++;
-        console.log(`Computer Won: ${computerChoice} beats ${humanChoice}`);
-        console.log(`You:${humanScore} Computer:${computerScore}`);
+
+        round.textContent = `${computerChoice} beats ${humanChoice}`;
+        score.textContent = `You:${humanScore} Computer:${computerScore}`;
+
 
     }
     else {
         humanScore++;
-        console.log(`You Won: ${humanChoice} beats ${computerChoice}`);
-        console.log(`You:${humanScore} Computer:${computerScore}`);
+
+        round.textContent = `${humanChoice} beats ${computerChoice}`;
+        score.textContent = `You:${humanScore} Computer:${computerScore}`;
+
     }
+
+    if (humanScore === 5) {
+
+        result.textContent = ` You Won the game  You:${humanScore}  Computer:${computerScore} `;
+
+
+    }
+    else if (computerScore === 5) {
+
+        result.textContent = `Computer Won the game You:${humanScore} Computer:${computerScore} `;
+
+    }
+
 }
+
+
+
+
 // playRound(getComputerChoice(), getHumanChoice());
 
-function playGame() {
-    let n = parseInt(prompt("Enter the number of rounds you want to play"));
-    for (let i = 0; i < n; i++) {
-        playRound(getComputerChoice(), getHumanChoice());
-    }
-    if (computerScore === humanScore) {
-        console.log("Game tied");
-        console.log(`You: ${humanScore} ${computerScore}`)
-    }
-    else if (computerScore > humanScore) {
-        console.log("Computer Won");
-        console.log(`You : ${humanScore} Computer: ${computerScore}`);
-    }
-    else {
-        console.log("You Won");
-        console.log(`You : ${humanScore} Computer: ${computerScore}`)
-    }
-}
-playGame();
+
